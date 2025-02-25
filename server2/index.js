cd348b4dff3bf23399f2338095628c533a4411bf
sohamlate <sohamslate2004@gmail.com>
2025-02-25 23:39:28 +0530

    final backend host change

--- server2/index.js
+++ server2/index.js
@@ -35,7 +35,16 @@ app.use('/api/extractimg', llmRoutes);
 app.use('/api/medicines', medicinesRoutes);
 app.use('/api/event' , eventRoute);
 
+app.get("/", (req, res) => {
+    return res.json({
+      success: true,
+      message: "Your server is up and running ..",
+    });
+  });
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = app;
